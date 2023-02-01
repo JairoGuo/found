721946197942e9bb8fd362d03f54e970bfb65361
Jairo Guo <jairoguo@163.com>
2023-02-01 19:47:22 +0800

    feat(infra): 字段属性异常；

---
+++ infra/src/main/java/com/jairoguo/infra/exception/FieldException.java
@@ -0,0 +1,7 @@
+package com.jairoguo.infra.exception;
+
+public class FieldException extends SystemException{
+    public FieldException(String msg) {
+        super(null, msg);
+    }
+}
--- infra/src/main/java/com/jairoguo/infra/exception/SystemException.java
+++ infra/src/main/java/com/jairoguo/infra/exception/SystemException.java
@@ -3,7 +3,7 @@ package com.jairoguo.infra.exception;
 /**
  * @author jairoguo
  */
-public final class SystemException extends BaseException{
+public class SystemException extends BaseException{
     public SystemException(String code, String msg) {
         super(code, msg);
     }
