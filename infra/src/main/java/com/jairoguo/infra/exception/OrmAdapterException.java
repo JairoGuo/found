65ab40719fc5104f00759f4f87b67742f48b1a80
Jairo Guo <jairoguo@163.com>
2023-02-25 17:34:54 +0800

    feat(infra): 自定义ORM适配异常与分页异常

---
+++ infra/src/main/java/com/jairoguo/infra/exception/OrmAdapterException.java
@@ -0,0 +1,10 @@
+package com.jairoguo.infra.exception;
+
+/**
+ * @author Guo Jinru
+ */
+public class OrmAdapterException extends SystemException{
+    public OrmAdapterException(String msg) {
+        super(null, msg);
+    }
+}
---
+++ infra/src/main/java/com/jairoguo/infra/exception/PageException.java
@@ -0,0 +1,10 @@
+package com.jairoguo.infra.exception;
+
+/**
+ * @author Guo Jinru
+ */
+public class PageException extends SystemException{
+    public PageException(String msg) {
+        super(null, msg);
+    }
+}
